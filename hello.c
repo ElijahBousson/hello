4d3f937185394f60be568ed7a17522c0652b51f5
Elijah <ebousson2@huskers.unl.edu>
2022-08-25 22:15:48 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Elijah Bousson
+ * Date: 2022/23/8
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Elijah Bousson\n");
+  return 0;
+}
